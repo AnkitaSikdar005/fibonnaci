eab475a58f6562698ade5c9f46ae92e604e2ec00
AnkitaSikdar005 <ank.sikdar2003@gmail.com>
2022-10-17 15:48:11 +0530

    22

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,39 +1,16 @@
-package fibonacci;
-import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
 
-public class Fibonacci {
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
 
-    public static void main(String[] args) {
-        System.out.print("input: ");
-        int number = new Scanner(System.in).nextInt();
- 
-        System.out.println("\nOutput");
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
 
-        for(int i=1; i<=number; i++){
-            System.out.print(fibRec(i) +" ");
-        }
-    } 
- 
-    public static int fibRec(int number){
-        if(number == 1 || number == 2){
-            return 1;
-        }
- 
-        return fibRec(number-1) + fibRec(number -2);
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
     }
- 
-    public static int fibonacciLoop(int number){
-        if(number == 1 || number == 2){
-            return 1;
-        }
-        int fibo1=1, fibo2=1, fibonacci=1;
-        for(int i= 3; i<= number; i++){
-            fibonacci = fibo1 + fibo2;
-            fibo1 = fibo2;
-            fibo2 = fibonacci;
- 
-        }
-        return fibonacci;
-    }     
-
+  }
 }
